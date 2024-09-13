b7e2d3bca403d372620e1ddfc59dd53136878bdb
Bhavishya Parmar <parmarbhavishya@gmail.com>
2024-09-13 11:24:35 +0530

    first commit

---
+++ IMAGES/app.js
@@ -0,0 +1,11 @@
+let userScore = 0;
+let compScore =0;
+
+const choices = document.querySelectorAll(".choice");
+
+choices.forEach((choice) =>
+{
+    choice.addEventListener("click" , () => {
+        
+    });
+});
